7b495d9217ee03bc0a4ab63b694a31a6fba3e257
JACKSON <gabbyjax7@gmail.com>
2025-05-19 02:54:26 +0100

    fixing cors

--- server.js
+++ server.js
@@ -17,18 +17,34 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-const allowedOrigins = process.env?.ALLOWED_ORIGINS?.split(",");
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://truststock.vercel.app"
+];
 
 app.use(cors({
     origin: (origin, callback) => {
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+    } else {
+        callback(new Error("Not allowed by CORS"));
+    }
     },
-    credentials: true                // Allow cookies to be sent
+    credentials: true
 }));
+
+
+// const allowedOrigins = process.env?.ALLOWED_ORIGINS?.split(",");
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if(!origin || allowedOrigins.includes(origin)){
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     credentials: true                // Allow cookies to be sent
+// }));
 app.use(cookieParser());
 
 //Routes
