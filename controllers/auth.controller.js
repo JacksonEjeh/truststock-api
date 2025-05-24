e46608ff3d94eadb4183fbc8586c4bccc6505b1b
JACKSON <gabbyjax7@gmail.com>
2025-05-24 14:51:38 +0100

    cookie setup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -197,8 +197,8 @@ const signIn = async (req, res, next) => {
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+            secure: true,
+            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
